d07192b85b5ed550084b33ef74cb1626ff209c50
DagimDev <simedagim121@gmail.com>
2025-05-22 23:28:57 +0300

    Find Hidden Word

--- scripts.js
+++ scripts.js
@@ -124,42 +124,137 @@ console.log("Algorithm Practice");
 // •	Loop through each character and check if it’s a vowel.
 
 
-const CountVowels = (s) => {
-  let vowel = "";
-  let cc = 0
-  for (i = 0; i < s.length; i++) {
-    if (s[i] == 'a') {
-      vowel += s[i]
-      cc += i
-    } 
-    else if (s[i] == 'e') {
-      vowel += s[i]
+// const CountVowels = (s) => {
+//   let vowel = "";
+//   let cc = 0
+//   for (i = 0; i < s.length; i++) {
+//     if (s[i] == 'a') {
+//       vowel += s[i]
+//       cc += i
+//     } 
+//     else if (s[i] == 'e') {
+//       vowel += s[i]
+//     }
+//     else if (s[i] == 'i') {
+//       vowel += s[i]
+//     }
+//     else if (s[i] == 'o') {
+//       vowel += s[i]
+//     }
+//     else if (s[i] == 'u') {
+//       vowel += s[i]
+//     }
+//   }
+//   return vowel
+// }
+
+// const variable = "palindrome";
+// const count = CountVowels(variable);
+// console.log(count)
+
+
+// function countVowels(str) {
+//     let count = 0;
+//     const vowels = "aeiou";
+//     for (let char of str.toLowerCase()) {
+//         if (vowels.includes(char)) count++;
+//     }
+//     return count;
+// }
+
+// console.log(countVowels("palindrome"))
+
+
+// What's Hiding Amongst the Crowd?
+
+// A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function to detect what the word is, knowing the following rules:
+
+// The wanted word is in lowercase.
+// The crowd of letters is all in uppercase.
+// Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
+
+// Examples
+// detectWord("UcUNFYGaFYFYGtNUH") ➞ "cat"
+
+// detectWord("bEEFGBuFBRrHgUHlNFYaYr") ➞ "burglar"
+
+// detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment") ➞ "embezzlement"
+
+// Steps to follow when solving any kind of JavaScript problem:
+//   1. Understand the problem
+
+//   2. Solve it on paper
+//     - Use example cases to make sure that you can actually solve the problem on paper
+
+//       NOTE: The most difficult part for new comers is not actually solving the problem. The difficulty is in understanding & identifying the smallest steps the brain takes to solve the problem. (Our brain is sometimes too fast for us to comprehend)
+
+//   3. Craft a general step that can be followed to solve any kind of similar problem
+
+//     - This is where you would consider scenarios that might not have been covered in the examples you solve above and tweak the solution accordingly
+
+//   4. Write the pseudo code
+
+//     - Identify the smallest steps to follow and pick what JavaScript functionality you need to use to cover that step
+
+//   5. Translate to JavaScript
+//     - Follow the pseudo code and write the solution
+
+//   6. Test
+
+// Solution:
+// - I need something to save the hidden letters when found
+//   Declare a variable = hiddenLetter = ''
+
+// - if empty (return no value message)
+//   if (strValue) {
+//     //do something
+//   }
+
+// - check if given thing is a string to begin with
+
+//   if (typeof strValue === 'string') {
+//     // this is a string
+//   }else{
+//     if not return some message
+//   }
+// Use loop
+// for (0, strValue.length, ++ )
+// 1. find first letter
+//   strValue.charAt(0);
+// 2. check if uppercase
+// convert to uppercase using toUpperCase()
+// compare with the original
+// if equal, it was uppercase. If not it was lowercase
+
+//   if lowercase save in memory
+//     Add to hiddenLetter
+// 3. move to the next letter
+//   if lowercase add to the saved memory
+//   if upper case ignore
+//   We need to loop through the strings
+
+//     use the string property strValue.length
+//     for (0, strValue.length, ++ )
+// 4. Continue to the next letter
+//   repeat the same thing
+
+//   return hiddenLetter
+
+function findHiddenWord(a) {
+  var strValue = a;
+  var hiddenLetter = "";
+  if (strValue) {
+    if (typeof strValue === "string") {
+      for (var i = 0; i < strValue.length; i++) {
+        if (strValue.charAt(i) !== strValue.charAt(i).toUpperCase()) {
+          hiddenLetter = hiddenLetter + strValue.charAt(i);
+        }
+      }
+    } else {
+      return "Please enter a String value";
     }
-    else if (s[i] == 'i') {
-      vowel += s[i]
-    }
-    else if (s[i] == 'o') {
-      vowel += s[i]
-    }
-    else if (s[i] == 'u') {
-      vowel += s[i]
-    }
+  } else {
+    return "Please enter a value first";
   }
-  return vowel
+  return hiddenLetter;
 }
-
-const variable = "palindrome";
-const count = CountVowels(variable);
-console.log(count)
-
-
-function countVowels(str) {
-    let count = 0;
-    const vowels = "aeiou";
-    for (let char of str.toLowerCase()) {
-        if (vowels.includes(char)) count++;
-    }
-    return count;
-}
-
-console.log(countVowels("palindrome"))
