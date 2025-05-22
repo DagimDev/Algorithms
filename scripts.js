b94516d1139932b4b1e84b98393356fa47da540b
DagimDev <simedagim121@gmail.com>
2025-05-22 23:53:20 +0300

    Move addUp to Easy

--- scripts.js
+++ scripts.js
@@ -42,19 +42,6 @@ console.log("Algorithm Practice");
 // const bubble = BubbleSort([12, 9, 5, 33, 89]);
 // console.log(bubble);
 
-// Create a function that takes a number as an argument. Add up all the numbers from 1 to the
-// number you passed to the function. For example, if the input is 4 then your function should
-// return 10 because 1 + 2 + 3 + 4 = 10.
-// function addUp(num) {
-//   let int = 0;
-//   for (i = 0; i <= num; i++) {
-//     int += i;
-//   }
-//   return int;
-// }
-
-// console.log(addUp(4));
-
 // 1. Find the Maximum Number in an Array
 // 🧠 Problem:
 // Given an array, return the largest number.
