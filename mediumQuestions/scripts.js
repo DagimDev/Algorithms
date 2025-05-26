63e6cd2a59d3bef2b41ca106268ef735dece2146
DagimDev <simedagim121@gmail.com>
2025-05-26 22:35:01 +0300

    Roman to integer

--- mediumQuestions/scripts.js
+++ mediumQuestions/scripts.js
@@ -324,3 +324,16 @@ function firstNonRepeatingChar(str) {
 }
 
 console.log(firstNonRepeatingChar("aabbcdde")); // "c"
+
+// 27. Roman to Integer
+function romanToInt(s) {
+  const map = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000};
+  let total = 0;
+  for (let i = 0; i < s.length; i++) {
+    let curr = map[s[i]], next = map[s[i+1]];
+    total += (next > curr) ? -curr : curr;
+  }
+  return total;
+}
+
+console.log(romanToInt("XIV")); // 14
