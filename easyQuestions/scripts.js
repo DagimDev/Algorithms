9424af048fe67d411a54a9b5fcd5d5fafa994902
DagimDev <simedagim121@gmail.com>
2025-05-26 22:22:55 +0300

    Count character occurrences

--- easyQuestions/scripts.js
+++ easyQuestions/scripts.js
@@ -145,3 +145,19 @@ const firstArray = [1, 3, 5];
 const secondArray = [2, 4, 6];
 const sorted = MergeArrays(firstArray, secondArray);
 console.log(sorted);
+
+
+
+
+// 25. Count Character Occurrences
+function countChar(str, target) {
+  let count = 0;
+  for (let char = 0; char < str.length; char++) {
+    if (str[char] === target) {
+      count++
+    } 
+  }
+  return count;
+}
+
+console.log(countChar("hello world", "l")); // 3
