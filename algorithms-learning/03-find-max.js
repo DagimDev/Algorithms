d4c67199114543c99a6e7090b602d981866df57a
DagimDev <simedagim121@gmail.com>
2025-05-29 01:09:58 +0300

    feat: Implement max number finder in JavaScript

---
+++ algorithms-learning/03-find-max.js
@@ -0,0 +1,8 @@
+// Example Algorithm: Find Maximum of Two Numbers
+
+function findMax(a, b) {
+  // Compare two numbers and return the larger one
+  return a > b ? a : b;
+}
+
+console.log(findMax(7, 12)); // 12
