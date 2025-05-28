cb60f33965058c1ea66e4ab3dfb2515e6bbcf4c1
DagimDev <simedagim121@gmail.com>
2025-05-28 22:38:08 +0300

    feat: add two-sum solution using hash set

--- hardQuestions/scripts.js
+++ hardQuestions/scripts.js
@@ -1,1 +1,17 @@
 console.log("Hard Questions")
+
+// 1. Initial Implementation (Two-Number Sum)
+// twoSum.js
+function hasTwoSum(nums, target) {
+  const seen = new Set();
+  for (const num of nums) {
+    const complement = target - num;
+    if (seen.has(complement)) {
+      return true;
+    }
+    seen.add(num);
+  }
+  return false;
+}
+
+module.exports = hasTwoSum;
