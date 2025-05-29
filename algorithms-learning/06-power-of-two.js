172e38c887a0d5f24fb66e711d9c38f6d7cabae2
DagimDev <simedagim121@gmail.com>
2025-05-29 16:33:58 +0300

    perf: Optimize power-of-two check to O(1) with bitwise operation

--- algorithms-learning/06-power-of-two.js
+++ algorithms-learning/06-power-of-two.js
@@ -1,14 +1,14 @@
-const poweroftwo = (n) => {
-   if (n < 1) {
-    return false;
-   } 
-   for (let i = n; i > 1; i = i / 2) {
-    if (i % 2 !== 0) {
-        return false
-    }
-   }
-   return true
-}
+// const poweroftwo = (n) => {
+//    if (n < 1) {
+//     return false;
+//    } 
+//    for (let i = n; i > 1; i = i / 2) {
+//     if (i % 2 !== 0) {
+//         return false
+//     }
+//    }
+//    return true
+// }
 
 // const poweroftwo = (n) => {
 //    if (n < 1) {
@@ -23,7 +23,10 @@ const poweroftwo = (n) => {
 //    return true
 // }
 
-console.log(poweroftwo(1))
-console.log(poweroftwo(2))
-console.log(poweroftwo(5))
-console.log(poweroftwo(6))
+const isPowerOfTwo = (n) => {
+    return n > 0 && (n & (n - 1)) === 0
+}
+console.log(isPowerOfTwo(1))
+console.log(isPowerOfTwo(2))
+console.log(isPowerOfTwo(5))
+console.log(isPowerOfTwo(8))
