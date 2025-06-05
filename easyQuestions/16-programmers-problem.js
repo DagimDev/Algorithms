de5e0d7450a3f0cf9fdde34434abec3af70ce800
DagimDev <simedagim121@gmail.com>
2025-06-05 23:54:16 +0300

    feat: The 3 Programmers Problem

---
+++ easyQuestions/16-programmers-problem.js
@@ -0,0 +1,30 @@
+// The 3 Programmers Problem
+
+// You hired three programmers and you (hopefully) pay them. Create a function that takes three numbers (the hourly wages of each programmer) and returns the difference between the highest-paid programmer and the lowest-paid.
+
+// Examples
+// programmers(147, 33, 526) ➞ 493
+
+// programmers(33, 72, 74) ➞ 41
+
+// programmers(1, 5, 9) ➞ 8
+
+const programmers = (a, b, c) => {
+    if (a > b && a > c && b > c){
+        return a - c
+    } else if (a > b && a > c && b < c) {
+        return a - b
+    } else if (b > a && b > c && a < c) {
+        return b - a
+    } else if (b > a && b > c && a > c) {
+        return b - c
+    } else if (c > a && c > b && a < b) {
+        return c - a
+    } else if (c > a && c > b && a > b) {
+        return c - b
+    }
+}
+
+console.log(programmers(147, 33, 526))
+console.log(programmers(33, 72, 74))
+console.log(programmers(1, 5, 9))
