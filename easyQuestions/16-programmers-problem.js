d3da7ab3fe578766f0baf28f08e4c2f8e3b29067
DagimDev <simedagim121@gmail.com>
2025-06-05 23:59:40 +0300

    Update The 3 Programmers Problem

--- easyQuestions/16-programmers-problem.js
+++ easyQuestions/16-programmers-problem.js
@@ -10,20 +10,28 @@
 // programmers(1, 5, 9) ➞ 8
 
 const programmers = (a, b, c) => {
-    if (a > b && a > c && b > c){
-        return a - c
-    } else if (a > b && a > c && b < c) {
-        return a - b
-    } else if (b > a && b > c && a < c) {
-        return b - a
-    } else if (b > a && b > c && a > c) {
-        return b - c
-    } else if (c > a && c > b && a < b) {
-        return c - a
-    } else if (c > a && c > b && a > b) {
-        return c - b
-    }
+    // if (a > b && a > c && b > c){
+    //     return a - c
+    // } else if (a > b && a > c && b < c) {
+    //     return a - b
+    // } else if (b > a && b > c && a < c) {
+    //     return b - a
+    // } else if (b > a && b > c && a > c) {
+    //     return b - c
+    // } else if (c > a && c > b && a < b) {
+    //     return c - a
+    // } else if (c > a && c > b && a > b) {
+    //     return c - b
+    // }
+
+    // more efficient code to solve this problem
+    const max = Math.max(a, b, c);
+    const min = Math.min(a, b, c);
+    return max - min
+    
 }
+// alterative solution
+// const programmers = (a, b, c) => Math.max(a, b, c) - Math.min(a, b, c);
 
 console.log(programmers(147, 33, 526))
 console.log(programmers(33, 72, 74))
