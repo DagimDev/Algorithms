16d27cfe105a6450e85ea3bd56633fedbc505a49
DagimDev <simedagim121@gmail.com>
2025-06-06 23:45:05 +0300

    feat: Kinetic Energy

---
+++ easyQuestions/33-kinetic-energy.js
@@ -0,0 +1,26 @@
+// Kinetic Energy  Kinetic energy can be calculated with the following formula:
+
+// KE = 1/2mv²
+
+// m is mass in kg
+// v is velocity in m/s
+// KE is kinetic energy in J
+// Return the Kinetic Energy in Joules, given the mass and velocity. For the purposes of this challenge, round answers to the nearest integer.
+
+// Examples
+// kineticEnergy(60, 3) ➞ 270
+
+// kineticEnergy(45, 10) ➞ 2250
+
+// kineticEnergy(63.5, 7.35) ➞ 1715
+// Notes
+// Expect only positive numbers for inputs.
+
+const kineticEnergy = (m, v) => {
+  return Math.round(0.5 * m * v * v);
+};
+
+// Test cases
+console.log(kineticEnergy(60, 3));    // 270
+console.log(kineticEnergy(45, 10));   // 2250
+console.log(kineticEnergy(63.5, 7.35)); // 1715
