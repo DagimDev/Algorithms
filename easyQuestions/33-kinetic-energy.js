cc788627db21529ae1d9acc95f0425c444ab1c5e
DagimDev <simedagim121@gmail.com>
2025-06-06 23:46:14 +0300

    Update: Kinetic Energy (Alternative solution)

--- easyQuestions/33-kinetic-energy.js
+++ easyQuestions/33-kinetic-energy.js
@@ -24,3 +24,8 @@ const kineticEnergy = (m, v) => {
 console.log(kineticEnergy(60, 3));    // 270
 console.log(kineticEnergy(45, 10));   // 2250
 console.log(kineticEnergy(63.5, 7.35)); // 1715
+
+const kineticEnergy1 = (mass, velocity) => {
+  const kineticEnergyValue = 0.5 * mass * velocity ** 2;
+  return Math.round(kineticEnergyValue);
+};
