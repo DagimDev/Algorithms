0a5c372ea8a178a21fcc4416a90192a4b1e4c27d
DagimDev <simedagim121@gmail.com>
2025-06-17 01:02:27 +0300

    Crazy function

---
+++ easyQuestions/18-crazy-function.js
@@ -0,0 +1,18 @@
+// Come Check Out This Crazy Function
+// You are given two numbers, a and b. Write a function which uses an expression to return the expected output. I am not going to tell you what the expression is because that will spoil the fun! :)
+
+// Examples
+// crazyfunction(10, 20) ➞ 30
+
+// crazyfunction(17, 35) ➞ 50
+
+// crazyfunction(61, 233) ➞ 212
+// Notes
+// Hint: Notice the tags?
+
+const crazyfunction = (a, b) => {
+    return a ^ b
+}
+console.log(crazyfunction(10, 20))
+console.log(crazyfunction(17, 35))
+console.log(crazyfunction(61, 233))
