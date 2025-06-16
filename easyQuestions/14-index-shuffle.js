de3811635ecd675278b20d7d093e6da7b3a03551
DagimDev <simedagim121@gmail.com>
2025-06-16 22:24:27 +0300

    Index shuffle

---
+++ easyQuestions/14-index-shuffle.js
@@ -0,0 +1,29 @@
+// Index Shuffle
+// Write a function that takes all even-indexed characters and odd-indexed characters from a string and concatenates them together.
+
+// To illustrate:
+
+// indexShuffle("abcd") ➞ "acbd"
+// // "ac" (even-indexed) + "bd" (odd-indexed)
+// Examples
+// indexShuffle("abcdefg") ➞ "acegbdf"
+
+// indexShuffle("holiday") ➞ "hldyoia"
+
+// indexShuffle("maybe") ➞ "myeab"
+// Notes
+// 0 should be treated as an even number.
+
+const indexShuffle = (str) => {
+    let even = []
+    let odd = []
+    for (let i = 0; i < str.length; i++) {
+            i % 2 === 0 ? even.push(str[i]) : odd.push(str[i])
+    }
+    return [...even, ...odd].join("")
+}
+
+console.log(indexShuffle("abcd"))
+console.log(indexShuffle("abcdefg"))
+console.log(indexShuffle("holiday"))
+console.log("")
