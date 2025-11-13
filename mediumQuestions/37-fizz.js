fefe5c54b259145a6d55c1568f43f868356f2eff
Dagim Sime <simedagim121@gmail.com>
2025-11-13 20:59:39 +0300

    two sum

--- mediumQuestions/37-fizz.js
+++ mediumQuestions/37-fizz.js
@@ -13,3 +13,16 @@
     }
 }
 Fizzbuzz(100);
+
+const twoSum = (arr, target) => {
+  // let index = []
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        return [i, j];
+        //  index.push(i, j)
+      }
+    }
+  }
+  return [];
+};
